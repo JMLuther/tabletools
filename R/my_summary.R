d4b3d4eba25e7d77f61579768cdc4fd7d6559bbd
JMLuther <james.luther@vanderbilt.edu>
2018-07-10 09:31:31 -0500

    update package

--- R/my_summary.R
+++ R/my_summary.R
@@ -4,6 +4,9 @@
 #' single continuous variable in a dataframe. Useful for grouped data summaries.
 #' @param df A dataframe
 #' @param my_var A variable name
+#' @param probs For quantile functions, a vector with the desired quantiles...c(0.025, 0.975) default
+#' @param na.rm a logical value indicating whether NA values should be stripped
+#' @param digits integer indicating number of decimal places
 #' @return A dataframe with named variables for: mean, sd, n, sem, and 95%CI
 #' @seealso \code{\link[dplyr]{summarise}}, \code{\link[base]{mean}}, \code{\link[base]{sd}},
 #'  \code{\link[base]{round}}, \code{\link[stats]{quantile}}
@@ -14,7 +17,7 @@
 #' mtcars %>% group_by(cyl) %>% my_summary(mpg)
 #' mtcars %>% group_by(cyl) %>% my_summary(mpg, probs = c(0.01, .999), digits = 0)
 
-my_summary <- function(df, my_var, probs = c(0.025, .975), digits = 2, ...) {
+my_summary <- function(df, my_var, probs = c(0.025, .975), na.rm = T, digits = 2, ...) {
   my_var <- enquo(my_var)
   mean_name <- paste0(quo_name(my_var), "_mean")
   sd_name   <- paste0(quo_name(my_var), "_sd")
@@ -24,11 +27,11 @@ my_summary <- function(df, my_var, probs = c(0.025, .975), digits = 2, ...) {
   ciu_name  <- paste0(quo_name(my_var), "_ci_", gsub("^[0-9]+.([0-9]+)", "\\1", probs[[2]])) # allows for flexible quantiles
 
   summarise(df,
-            !!mean_name := round(mean(!!my_var, na.rm=T), digits),
-            !!sd_name   := round(sd(!!my_var, na.rm=T), digits),
+            !!mean_name := round(mean(!!my_var, na.rm = na.rm), digits),
+            !!sd_name   := round(sd(!!my_var, na.rm=na.rm), digits),
             !!n_name    := sum(!is.na(!!my_var)), # an integer
-            !!sem_name  := round(sd(!!my_var, na.rm=T)/sqrt(sum(!is.na(!!my_var))), digits),
-            !!cil_name  := round(quantile(!!my_var, probs = probs[[1]], names = F, na.rm = T), digits),
-            !!ciu_name  := round(quantile(!!my_var, probs = probs[[2]], names = F, na.rm = T), digits)
+            !!sem_name  := round(sd(!!my_var, na.rm=na.rm)/sqrt(sum(!is.na(!!my_var))), digits),
+            !!cil_name  := round(quantile(!!my_var, probs = probs[[1]], names = F, na.rm = na.rm), digits),
+            !!ciu_name  := round(quantile(!!my_var, probs = probs[[2]], names = F, na.rm = na.rm), digits)
   )
 }
