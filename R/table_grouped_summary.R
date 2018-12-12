2f82fec2538f56153f0b3c4c3b3de694bed9c464
JMLuther <james.luther@vanderbilt.edu>
2018-12-12 15:55:33 -0600

    add examples

--- R/table_grouped_summary.R
+++ R/table_grouped_summary.R
@@ -7,25 +7,59 @@
 #' @param html_output should the result be returned as an htmlTable (if `html_table`` = TRUE) or as a dataframe (default)  
 #' @param total should an overal total column be included (default) or omitted
 #' @return A text table with grouped summary
-#' @seealso  \code{\link[base]{sprintf}}, \code{\link[base]{mean}}, \code{\link[base]{sd}}, \code{\link[htmlTable]{htmlTable}} 
+#' @seealso  \code{\link[base]{sprintf}}, \code{\link[base]{mean}}, \code{\link[stats]{sd}}, \code{\link[htmlTable]{htmlTable}} 
 #' @export
-
+#' @examples
+#' library(tidyverse)
+# # base(ish) R example 
+# # doesn't include the total
+# aggregate(mtcars[, c("disp", "hp", "drat", "mpg", "wt")],
+#'           by = list(cyl = mtcars[, "cyl"]),
+#'           FUN = function(x) sprintf("%.2f &pm %.2f", mean(x), sd(x))) %>% 
+#'   t() %>% 
+#'   as.data.frame() %>% 
+#'   rownames_to_column() %>% 
+#'   {names(.) <- unlist(.[1, ]); .} %>% 
+#'   .[-1, ] 
+#' 
+#' # output HTML format
+#' mtcars %>% 
+#'   table_grouped_summary(., 
+#'                         var_names =  c("disp", "hp", "drat", "mpg", "wt"),
+#'                         grouping_variable = "cyl",
+#'                         html_output = TRUE,
+#'                         total = TRUE)
+#' 
+#' # output text format
+#'   mtcars %>% 
+#'   table_grouped_summary(., 
+#'                         var_names =  c("disp", "hp", "drat", "mpg", "wt"),
+#'                         grouping_variable = "cyl",
+#'                         html_output = FALSE,
+#'                         total = TRUE)
+#' 
+#' # without overall total
+#' mtcars %>% 
+#'   table_grouped_summary(., 
+#'                         var_names =  c("disp", "hp", "drat", "mpg", "wt"),
+#'                         grouping_variable = "cyl",
+#'                         html_output = FALSE,
+#'                         total = FALSE)
+ 
 
 table_grouped_summary <- function(df, var_names, grouping_variable, html_output = FALSE, total = TRUE, ...) {
   t_sub =
     t(aggregate(df[, var_names], 
-                df[, grouping_variable], 
-                FUN = function(x) sprintf("%0.2f &pm; %0.2f (%.0f)", 
+                by = list(group = df[, grouping_variable]), 
+                FUN = function(x) sprintf("%0.2f &pm; %0.2f", 
                                           mean(x, na.rm = T), 
-                                          sqrt(var(x, na.rm = T)/sum(!is.na(x))), 
-                                          sum(!is.na(x)))))
+                                          sqrt(var(x, na.rm = T)/sum(!is.na(x))))))
   ifelse(total == TRUE, {
     t_total =
       as.matrix(unlist(lapply(df[, var_names],
-                              FUN = function(x) sprintf("%0.2f &pm; %0.2f (%.0f)",
+                              FUN = function(x) sprintf("%0.2f &pm; %0.2f",
                                                         mean(x, na.rm = T),
-                                                        sqrt(var(x, na.rm = T)/sum(!is.na(x))),
-                                                        sum(!is.na(x))))))
+                                                        sqrt(var(x, na.rm = T)/sum(!is.na(x)))))))
     t_total =
       rbind(c("TOTAL"),
             t_total)
@@ -42,3 +76,4 @@ table_grouped_summary <- function(df, var_names, grouping_variable, html_output 
                                                       matrix("padding:0 5px 0 5px;", ncol=ncol(t_out), nrow=nrow(t_out))),
                                      ...)))
 }
+
