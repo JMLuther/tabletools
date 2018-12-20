c922eecbc877a1cbe1c82b3c595d4621e932041f
JMLuther <james.luther@vanderbilt.edu>
2018-12-20 12:33:22 -0600

    add function gls_pvalue

---
+++ R/gls_pvalue.R
@@ -0,0 +1,14 @@
+#' Get p-value from an rms::Gls model
+#'
+#' @usage gls_pvalue(gls_model)
+#' @param gls_model a Gls model obtained from \code{\link[rms]{Gls}}
+#' @return A single p-value
+#' @seealso  \code{\link[rms]{Gls}}
+#' @export
+
+
+gls_pvalue <- function(gls_model) {
+  p.value = 2 * pt(-abs(coef(gls_model)/sqrt(diag(as.matrix(gls_model$varBeta)))), gls_model$dims$N - gls_model$dims$p)[2] 
+  names(p.value) = "p.value"
+  return(p.value)
+}
