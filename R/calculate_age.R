843c1a68d62b611c24ad6f4d9516b34d6086ed87
JMLuther <james.luther@vanderbilt.edu>
2024-06-19 13:37:44 -0500

    add age calc function

---
+++ R/calculate_age.R
@@ -0,0 +1,32 @@
+#' Calculate age from date of birth
+#'
+#' @param dob Date of birth, preferably "YYYY-MM-DD" format
+#' @param date Date to calculate age from, default is today
+#' @param date_format defaults to "ymd" "YYYY-MM-DD" format
+#' @param units units to return age in, default is "years"; other options are "days", "weeks", "months"
+#'
+#' @return age in years
+#' @export
+#'
+#' @examples
+#' calculate_age(dob = "8/12/1975", date_format = "mdy")
+#' calculate_age(dob = "1975-08-12", date_format = "ymd")
+#' calculate_age(dob = "1975-08-12", date_format = "ymd", units = "days")
+#' calculate_age(dob = "1975-08-12", date_format = "ymd", units = "weeks")
+
+calculate_age <- function(dob, date="today", date_format="ymd", units="years") {
+  if (date=="today") {
+    date = Sys.Date()
+  } else if (date_format=="ymd") {
+    date = lubridate::ymd(date)
+  } else if (date_format=="mdy") {
+    date = lubridate::mdy(date)
+  }
+  
+  dob = switch(date_format,
+               "mdy" = lubridate::mdy(dob),
+               "ymd" = lubridate::ymd(dob))
+
+  age = as.numeric(interval(dob, d), {{units}})
+  return(age)                 
+}
