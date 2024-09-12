9d973e1c996ef233dde1002e6bf6e2428475b01d
JMLuther <james.luther@vanderbilt.edu>
2024-09-12 16:23:34 -0500

    add clean_author_list; not working completely

---
+++ R/clean_author_list.R
@@ -0,0 +1,43 @@
+#' Reformat list of authors into Bibliography format
+#'
+#' Reformats from typical list of full names (First middle Last,...) to standard
+#' bibliographic format (Last FM, ...). Strips out numbers. Will not work for
+#' some double and/or hyphenated names or other edge cases which will still have
+#' to be cleaned manually.
+#'
+#' @param string_authors A text list of authors
+#'
+#' @return String of reformatted names
+#' @export
+#'
+#' @examples
+#' authors <- "Desmaré van Rooyen1,2, Sascha Bandulik3, Chandan Kumar-Sinha4,5, 
+#'   Aaron M. Udager4,5,6, Chaelin Lee2, Heather Wachtel7, Debbie L. Cohen8, 
+#'   James M. Luther9, Thomas Giordano6, Adina Turcu2, Richard Warth3, 
+#'   William E. Rainey1,2 and Juilee Rege*1"
+#' clean_author_list(authors)
+#' auth <- "B. Ilkin Safa1, Jared Oakes1, Leslie Kirk1, Eric Olson1, 
+#' Christian M. Warren1, Erin M. Wilfong1, Matthew T. Stier1, Samuel S. Bailin1,
+#'  Curtis Gabriel1, Jeffrey C. Rathmell1, C. Robb Flynn1, J. Matthew Luther1, 
+#'  John R. Koethe1,2, Spyros A. Kalams1, Celestine N. Wanjalla1, Mona Mashayekhi1"
+#' clean_author_list(auth) 
+
+
+
+clean_author_list <- function(string_authors){
+  # require(stringr)
+  # remove numbers and whitespace
+  au1 <- gsub("[0-9\\*]+", "", trimws(string_authors))
+  au1 <- gsub("[ ]*and[ ]*", "", au1)
+  author_full = trimws(regmatches(au1, gregexpr("[^,]+", au1))[[1]])
+  name_last =  gsub(".* ([A-Za-z-]+)$", "\\1", author_full) # doesn't handle van
+  init_first =  gsub("^([A-Z]).* .*", "\\1", author_full)
+  # originally used stringr package but can be done in base R
+  init_middle = trimws(gsub("\\.", "", stringr::str_extract(author_full, " ([A-Z])\\.")))
+  # init_middle = trimws(gsub("\\.", "", regmatches(author_full, m = regexpr(" ([A-Z])\\.", author_full))))
+  init_middle[is.na(init_middle)] = ""
+  name_inits = paste0(name_last, " ", init_first, init_middle)
+  # data.frame(author_full, name_last, init_first, init_middle, name_inits) # checking
+  paste0(name_inits, collapse = ", ") # return value
+}
+
