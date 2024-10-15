8941cd703770646e945caace955f2430cc15a711
JMLuther <james.luther@vanderbilt.edu>
2024-10-15 16:10:21 -0500

    standardize mgfr functions

--- R/calculate_mgfr_msp.R
+++ R/calculate_mgfr_msp.R
@@ -8,36 +8,10 @@
 #' Consortium statment paper}.
 #' The function can be compared against an excel spreadsheet provided in the
 #' supplemental material.
-#'
-#' @param time A vector of time values (minutes)
-#' @param iohexol A vector of Iohexol concentrations (mcg/mL) determined at the
-#'   provided times. These values should not be log transformed
-#' @param omnipaque_v Omnipaque version (eg 300 = Omnipaque 300 recommended).
-#'   Other versions commonly available include 300 and 350 (USA) and 140, 280,
-#'   240 (other areas)
-#' @param ioh_inj_wt Iohexol injection Weight by syringe weight determination
-#'   (Pre-Post weight difference; recommnded method) in grams
-#' @param ioh_inj_vol Iohexol injection Volume by syringe volume injected (Not
-#'   preferred; use if weights not available)
-#' @param height Patient Height, cm
-#' @param weight Patient Weight, kg
-#' @param height_units Height units, if not in cm
-#' @param weight_units Weight units, if not in kg
-#'
+#' 
+#' @inheritParams calculate_mgfr_2c
 #' @md
-#' @return Data.frame containing results with calculated values for the
-#'   following variables:
-#'
-#' *  `iohexol_m` Iohexol dose (mcg Iodine)
-#' *  `iohexol_0` Iohexol intercept (mcg/mL)
-#' *  `iohexol_k` Iohexol elimination constant (k)
-#' *  `rsq` Iohexol model R-sqaured value (recommend > 0.97)
-#' *  `ioh_auc` Iohexol area under the curve (AUC, mcg min / mL)
-#' *  `ioh_CL` Iohexol Clearance (dose/area)
-#' *  `mGFR_1c` GFR (mL/min) determined by Iohexol clearance
-#' *  `mGFR_1c_bsa` GFR indexed to BSA (mL/min/1.73 m2 bsa)
-#' @export calculate_mgfr_msp
-#'
+#' @export
 #' @examples
 #' # XLSX example data
 #' df1_dem <- data.frame(height=168,     # cm
@@ -77,39 +51,88 @@
 #'                                             height = height, weight = weight))) |> 
 #'  tidyr::unnest(mgfr)
 
-calculate_mgfr_msp <- function(time, iohexol, 
-                              omnipaque_v=300, # default
-                              ioh_inj_wt=NULL, ioh_inj_vol=NULL, # must give one of these
-                              height, weight, height_units="cm", weight_units="kg"){
+calculate_mgfr_msp <- function(time, iohexol_conc, 
+                               omnipaque_v=300, ioh_inj_vol=5.0,
+                               ioh_inj_wt=NULL,  
+                               ioh_units="ug/mL", time_units="min",
+                               id=NULL,
+                               height=NA, height_units = "m", 
+                               weight=NA, weight_units = "kg",
+                               method_adj = "BM", t_late = 120, #t_early = 100,
+                               output="summary"){
   # Subject calcs
-  bsa = calculate_bsa(weight, height, height_units = height_units, method = "DuBois")
+  bsa = ifelse(is.na(height) || is.na(weight), NA, 
+               calculate_bsa(weight, height, height_units = height_units, method = "DuBois"))
   
   # Omnipaqe calcs; from lookup table stored internally as tabletools:::df_omnipaque
+  # Note: standard 5mL injection of Iohexol iohexol_v = 5 # mL = 3235 mg Iohexol.
+  # df_omnipaque=tabletools:::df_omnipaque # available in internal data
   ioh_spgrav = df_omnipaque$omnipaque_specgrav[df_omnipaque$omnipaque_v==omnipaque_v]/1000 # g/ml
   iohexol_mg_ml = df_omnipaque$iohexol_mg_ml[df_omnipaque$omnipaque_v==omnipaque_v] # mg/ml
+  if (!is.null(ioh_inj_wt) & !is.null(ioh_inj_vol)) stop("If iohexol injection weight is provided, assign `ioh_inj_vol=NULL` and exact iohexol mass will be calculated.")
   if (is.null(ioh_inj_wt) & is.null(ioh_inj_vol)) stop("missing injection details; must provide `ioh_inj_wt` or `ioh_inj_vol`")
   if (!is.null(ioh_inj_wt)) {ioh_inj_vol = ioh_inj_wt/ioh_spgrav  } # vol calculated by weight
-  iohexol_m = ioh_inj_vol*iohexol_mg_ml*1000 # mcg
+  iohexol_m = ioh_inj_vol*iohexol_mg_ml*1000 # mass mcg iohexol injected
   
-  # regression details
-  ln_iohexol = log(iohexol) # ln of Iohexol (mcg/ml)
-  lm_mod = lm(ln_iohexol ~ time)
-  slope = coef(lm_mod)[["time"]]
-  intercept_log = coef(lm_mod)[["(Intercept)"]]
-  intercept = exp(intercept_log)
-  rsq = summary(lm_mod)$r.squared
-  ioh_auc = intercept/abs(slope)
-  ioh_CL = iohexol_m/ioh_auc
-  mGFR_1c = 0.990778*ioh_CL - 0.001218*ioh_CL^2 # Brochner-Mortenson correction
-  mGFR_1c_bsa = mGFR_1c/bsa*1.73
+  # Data: get time, iohexol data
+  time_min =  convert_time_to_min(time, time_units)
+  iohexol_valid = ifelse(tolower(ioh_units) %in% c("ug/ml","mcg/ml","mg/l"), T, NA)
+  iohexol = iohexol_conc[iohexol_valid]
   
-  return(data.frame("iohexol_m"   = iohexol_m,
-                    "iohexol_0"   = intercept,
-                    "iohexol_k"   = slope,
-                    "model_r2"    = rsq,
-                    "ioh_auc"     = ioh_auc,
-                    "ioh_CL"      = ioh_CL,
-                    "mgfr_1c"     = mGFR_1c,
-                    "mgfr_1c_bsa" = mGFR_1c_bsa
-  ))
-}
+  # get initial guess from SI method
+  dat = data.frame(time=time_min, iohexol=iohexol)
+  # wt <- if (nls_weights) {1/iohexol^2} else {rep(1, length(dat$iohexol))} # weights for NLLS
+  
+  # Late Timepoints 
+  # t_late = 120
+  dat <- dat[dat$time >= t_late, ]
+  lm_late <- lm(log(iohexol) ~ time, data = dat)
+  B <-  exp(coef(lm_late))[["(Intercept)"]]
+  b <- -coef(lm_late)[["time"]]
+  
+  AUC_inf <- B/b  # AUC_inf 
+  mgfr_late = iohexol_m/AUC_inf  # Dose/AUC_inf = mL/minutes (not indexed to BSA)
+  # Adjust for unmeasured early Iohexol conc (standard Brochner-Mortenson correction)
+  # can add other options for alternative adjustments
+    if (method_adj == "BM") {mgfr_msp = 0.990778*mgfr_late - 0.001218*mgfr_late^2}
+  mgfr_msp_bsa = mgfr_msp/bsa*1.73
+  mgfr_method = paste0("MSP-", method_adj)
+  
+  # mgfr_2c_bsa = ifelse(!is.na(bsa), mgfr_2c*1.73/bsa, NA)  # indexed to BSA
+  iohexol_vd = NA # iohexol_m/1000/(A+B) # volume of distribution, L
+  
+  # model fit parameters
+  # fit_SI_vals_late <- function(t, A, a, B, b) {A*exp(-a*t)+B*exp(-b*t) }
+  dat$pred <- predict(lm_late)
+  dat$resid <- exp(residuals(lm_late)) #  transform to reg scale
+  # ODE micro parameters:
+  k10 = NA # iohexol_m/iohexol_vd/AUC_inf/1000 # 1/min
+  k21 = NA #a*b/k10
+  k12 = NA #a+b - k10 - k21
+  
+  model_r2 =  summary(lm(pred ~ iohexol, data = dat))[["r.squared"]]  # this is a pseudo-r2
+  ssr = NA #sum(dat$resid^2)
+  sse = NA #sum(dat$resid[dat$time <= t_early]^2)
+  ssl = sum(dat$resid^2)
+  res = data.frame("mgfr_method" = mgfr_method,
+                   "mgfr_2c"     = mgfr_msp,
+                   "mgfr_2c_bsa" = mgfr_msp_bsa,
+                   "iohexol_m"   = iohexol_m,
+                   "iohexol_0"   = NA, # A+B,
+                   "iohexol_vd"  = iohexol_vd,
+                   "ioh_auc"     = AUC_inf,
+                   "A"           = NA, #A,
+                   "a"           = NA, #a,
+                   "B"           = B,
+                   "b"           = b,
+                   "model_r2"    = model_r2, # this is a pseudo-r2
+                   "ssr"         = ssr,
+                   "sse"         = sse,
+                   "ssl"         = ssl,
+                   "k10"         = k10,
+                   "k21"         = k21,
+                   "k12"         = k12,
+                   "n_early"     = NA, #length(dat_early$time),
+                   "n_late"     = length(dat$time))
+  return(res)
+  }
