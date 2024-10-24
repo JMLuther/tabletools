5bac974cca1fe0dda1ec14bd0141a0c9bb643c08
JMLuther <james.luther@vanderbilt.edu>
2024-10-24 17:18:59 -0500

    fix compare_mgfr plotting

--- R/compare_mgfr_summary.R
+++ R/compare_mgfr_summary.R
@@ -1,23 +1,30 @@
 #' Compare Iohexol mGFR results using available methods
 #'
-#' Useful to analyze outlier or inconsistent results. Note most variation occurs
-#' due to poor fitting of early timepoints. Further discussion and details
-#' regarding methods of fitting the 2-compartment models can be found at
+#' Two functions are provided to to summarize available options and analyze
+#' outlier or inconsistent results. Most variation occurs due to poor fitting or
+#' variability in early timepoints (<90min), although the late time points
+#' account for ~90% of the measured AUC used in calculation of creatinine
+#' clearance. Further discussion and details regarding methods of fitting the
+#' 2-compartment models can be found at
 #' \href{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8101203/}{Pottel et al.}
 #' The summary provides estimates of kinetic parameters (k10, k21, k12) which
 #' can be used in ODE models, but is not implemented here.
 #'
-#' @inheritParams calculate_mgfr_2c 
-#' @param ... Inherits from named `calculate_mgfr_2c` paramaters. Any arguments can be added, but must be named (eg `ioh_inj_vol = 5.1`).
+#' @inheritParams calculate_mgfr_2c
+#' @param ... Inherits from named `calculate_mgfr_2c` paramaters. Any arguments
+#'   can be added, but must be named (eg `ioh_inj_vol = 5.1`).
 #' @rdname compare_mgfr_summary
 #' @importFrom purrr pmap_df
 #'
-#' @return `compare_mgfr_summary()` Returns a table of mGFR summary parameters, using combination of available methods. The single sample method result is not included but can be called separately using `calculate_mgfr_ss`. ' @export '
-#' @export
+#' @return `compare_mgfr_summary()` Returns a table of mGFR summary parameters,
+#'   and `compare_mgfr_plot()` returns a panel of plots using a combination of available methods.
+#'   The single sample method result is not included but can be called
+#'   separately using `calculate_mgfr_ss`. '
+#' @export compare_mgfr_summary
 #' @examples
 #' library(tabletools)
 #' # PUBLISHED IOHEXOL DATA ----
-#' # data available in package by calling 
+#' # data available in package by calling
 #' ## ├ Schwartz Data ----
 #' # Data extracted from Schwartz Fig1 https://pubmed.ncbi.nlm.nih.gov/16612328/
 #' # Iohexol 5mL IV injection (Omnipaque 300, 5mL ~3235mg Iohexol)
@@ -26,7 +33,7 @@
 #' # Iohexol (ug/ml)
 #' # age, height, weight not known for the example
 #' dat_schwartz
-#' 
+#'
 #' ## ├ Pottel data ----
 #' # from Supplemental document in https://pubmed.ncbi.nlm.nih.gov/33952185/
 #' dat10
@@ -35,25 +42,25 @@
 #' ## ├ Tondel data ----
 #' # full example data provided by Tondel in Table 2: https://pubmed.ncbi.nlm.nih.gov/29134449/
 #' dat_tondel
-#' 
+#'
 #' compare_mgfr_summary(dat_tondel$time, dat_tondel$iohexol, ioh_inj_vol = 2.08, weight = 13, height = 0.9)
 #' compare_mgfr_plot(dat_tondel$time, dat_tondel$iohexol, ioh_inj_vol = 2.08, weight = 13, height = 0.9, id="dat_tondel")
-#' 
+#'
 #' # Comparisons of available methods: published studies
 #' # Schwartz data in kids; note don't have subject details (height, weight, inj_vol)
 #' compare_mgfr_summary(dat_schwartz$time, dat_schwartz$iohexol_ug_ml, height = 0.8, weight = 13, ioh_inj_vol = 2.08)
 #' compare_mgfr_plot(dat_schwartz$time, dat_schwartz$iohexol_ug_ml, height = 0.8, weight = 13, ioh_inj_vol = 2.08, id="dat_schwartz")
-#' 
+#'
 #' # Pottel ID#10 Difficult fit
 #' # Negative value using unconstrained parameters (NLLS-base-unweighted)
 #' # caused by early outlier data points (t=60 most likely)
 #' compare_mgfr_summary(dat10$time, dat10$iohexol, height = 1.67, weight = 70)
 #' compare_mgfr_plot(dat10$time, dat10$iohexol, height = 1.67, weight = 70, id="dat10")
-#' 
+#'
 #' # Pottel ID#17 Difficult fit
 #' compare_mgfr_summary(dat17$time, dat17$iohexol, height = 1.67, weight = 70, id="dat17")
 #' compare_mgfr_plot(dat17$time, dat17$iohexol, height = 1.67, weight = 70, id="dat17")
-#' 
+#'
 #' # ODE MODEL FOR COMPARISON;
 #' # Example only to verify ODE parameter estimates, not required for GFR calculation
 #' # This model uses solution from the nls model to get the kinetic parameters
@@ -78,26 +85,27 @@
 #' lines(out_df$time, out_df$Iohexol, lty=2)
 #' 
 compare_mgfr_summary  <- function(time, iohexol_conc, t_early=100, t_late=120,...) {
-  nls_vs = list(nls_v       = c("SI", "SI", "gslnls", "base", "gslnls", "base", "msp"),
-                nls_weights = c(  T,   T,        T,      T,        F,      F,    F),
-                t_early     = c(t_early, t_late, t_early, t_early, t_early, t_early, t_early),
+  nls_vs = list(nls_v       = c("gslnls", "gslnls","base", "base","SI", "SI",  "msp"),
+                nls_weights = c(       T,        F,     T,      F,   F,    F,     F),
+                t_early     = c(t_early, t_early, t_early, t_early, t_early, t_late, t_early),
                 t_late      = rep(t_late, 7))
+  # nls_vs = list(nls_v       = c("SI", "SI", "gslnls", "base", "gslnls", "base", "msp"),
+  #               nls_weights = c(  T,   T,        T,      T,        F,      F,    F),
+  #               t_early     = c(t_early, t_late, t_early, t_early, t_early, t_early, t_early),
+  #               t_late      = rep(t_late, 7))
   pmap_df(nls_vs, calculate_mgfr_2c, time=time, iohexol_conc=iohexol_conc, output="summary",...) 
 }
 
 
-#' @inheritParams calculate_mgfr_2c ' 
-#' @param ... Inherits from named `calculate_mgfr_2c` paramaters. Any arguments can be added, but must be named (eg `ioh_inj_vol = 5.1`).
-#' @return `compare_mgfr_plot()` returns a 3x2 grid of mGFR summary Plots, using combination of available methods. Note the `base` versions are not plotted but results can be reviewed using `compare_mgfr_summary` ' 
-#' @export
-#' @examples
+#' @inheritParams calculate_mgfr_2c
+#' @export compare_mgfr_plot
 compare_mgfr_plot  <- function(time, iohexol_conc, t_early=100, t_late=120,...) {
-  nls_vs = list(nls_v       = c("SI", "SI", "gslnls", "gslnls", "msp"),
-                nls_weights = c(  T,     T,        T,        F,    F),
-                t_early     = c(t_early, t_late, t_early, t_early, t_early),
-                t_late      = rep(t_late, 5))
-  opar_mfr = par("mfrow")
-  par(mfrow = c(3,2))
-  pmap_df(nls_vs, calculate_mgfr_2c, time=time, iohexol_conc=iohexol_conc, output="plot",...) 
-  par(mfrow=opar_mfr)
+  nls_vs = list(nls_v       = c("gslnls", "gslnls","base", "base","SI", "msp"),
+                nls_weights = c(       T,        F,     T,      F,   F,    F),
+                t_early     = c(t_early, t_early, t_early, t_early, t_early, t_early),
+                t_late      = rep(t_late, 6))
+  old.mfrow = par("mfrow")
+  par(mfrow=c(3,2))
+  pmap(nls_vs, calculate_mgfr_2c, time=time, iohexol_conc=iohexol_conc, output="plot",...) 
+  par("mfrow"=old.mfrow) 
 }
