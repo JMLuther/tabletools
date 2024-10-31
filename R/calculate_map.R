9d99f8b1ef51e500323d692c5e90ebc673b11a2b
JMLuther <james.luther@vanderbilt.edu>
2024-10-31 10:11:52 -0500

    add Gomez formula calculations

--- R/calculate_map.R
+++ R/calculate_map.R
@@ -1,19 +1,43 @@
-#' Calculate Mean Arterial Pressure 
-#' 
-#' Returns the mean arterial pressure (MAP) from standard systolic (SBP) and diastolic (DBP) blood pressure measurements
+#' Calculate Mean Arterial Pressure
+#'
+#' Returns the mean arterial pressure (MAP) from standard systolic (SBP) and
+#' diastolic (DBP) blood pressure measurements. Note it is best to record MAP
+#' reported directly by the device. Heart rate adjusted methods reported by
+#' \href{https://pubmed.ncbi.nlm.nih.gov/1860182/}{Gomez} and by
+#' \href{https://pubmed.ncbi.nlm.nih.gov/15558774/}{Razminia} are also
+#' supported. The Gomez method is applied as the default, for use in calculation
+#' in renal glomerular pressure estimates.
 #'
 #' @param sbp Systolic blood pressure, mmHg
 #' @param dbp Diastolic blood pressure, mmHg
+#' @param hr Heart rate, bpm
+#' @param method MAP formula, one of `gomez`, `standard`, or `razminia`. Uses
+#'   "gomez" method by default. If no HR is supplied, the "standard" method is
+#'   used. If `standard` method is used without specifying it, due to no HR, a
+#'   warning is issued.
 #'
-#' @return map as a single value, or vector of results 
+#' @return map as a single value, or vector of results
 #' @export calculate_map
 #'
 #' @examples
 #' calculate_map(159, 46)
 #' calculate_map("159", 46)
 #' calculate_map("159", NA)
+#' calculate_map(140, 90)
+#' calculate_map(140, 90, 70, method="standard")
+#' calculate_map(140, 90, 70, method = "gomez")
+#' calculate_map(140, 90, 70, method="razminia")
+#' 
+#' hr <- 40:125
+#' plot(hr, map_dbl(hr, ~calculate_map(125, 73, .x, method = "gomez")), "l", lty=1, col="black",
+#'      # xlim = c(30,150), ylim = c(85,100),
+#'      ylab = "Calculated MAP (mmHg)")
+#' title("Methods comparison for HR-adjusted MAP calculation")
+#' lines(hr, map_dbl(hr, ~calculate_map(125, 73, .x, method = "razminia")), lty=2, col="red")
+#' abline(h=calculate_map(125,73, method="standard"),lty=2, col="blue")
 
-calculate_map <- function(sbp, dbp){
+
+calculate_map <- function(sbp, dbp, hr=NA, method="gomez"){ 
   if (any(is.na(sbp) | any(is.na(dbp)))) {
     rlang::warn("Check for missing values in sbp, dbp")
     return(NA_real_)}
@@ -22,8 +46,27 @@ calculate_map <- function(sbp, dbp){
     rlang::warn("non-numeric values in sbp or dbp")
     return(NA_real_)}
   
-  map = sbp/3 + 2*dbp/3
-  return(map)
-} 
-
-
+  if (is.na(hr)){
+    rlang::warn("no HR value, defaulting to standard method")
+    map = 1/3*sbp + 2/3*dbp
+    return(map)
+  }
+  if (method == "standard"){
+    map = 1/3*sbp + 2/3*dbp
+    return(map)
+  }
+  if (method=="gomez"){
+    # for method see https://pubmed.ncbi.nlm.nih.gov/1860182/
+    # rho = fraction of PP ~ mean value during diastole
+    gamma = 2.3*sqrt(hr)/60
+    rho = 12*(3*gamma + 1)/100
+    k = 0.77*gamma + rho*(1-gamma)
+    map = dbp + k*(sbp-dbp)
+    return(map)
+  }
+  if (method == "razminia"){ 
+    # see https://pubmed.ncbi.nlm.nih.gov/15558774/
+    map = dbp + (0.33 + (hr*0.0012))*(sbp-dbp)
+    return(map)
+  } else {return(NA)}
+}
