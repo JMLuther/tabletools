e3a89d42d8fcc1a53c83207a8002a081bc9b2886
JMLuther <james.luther@vanderbilt.edu>
2024-11-04 16:19:27 -0600

    fix egfr man

--- R/utils.R
+++ R/utils.R
@@ -1,32 +1,35 @@
 # Utility Functions ----
 ## ├ Handle Sex/Gender consistently ----
 # Always return Female/Male as character
-handle_sex <- function(sex){
+handle_sex <- Vectorize(
+  function(sex){
   sx = tolower(sex)
   sx = ifelse(sx %in% c("female", "women","woman", "fem", "fe", "f"), "Female",
               ifelse(sx %in% c("male", "man", "men", "ma", "m"), "Male", NA_character_))
   sx
-}
+})
 # test_case <- c("M", "ma", "man", "FE", "MA", "F", NA)
 # handle_sex(test_case)
 
 
 # Always return Female/Male as character
-handle_race <- function(race){
+handle_race <- Vectorize(
+  function(race){
   rx = tolower(race)
   rx = ifelse(rx %in% c("white", "w", "caucasian", "cauc."), "White",
               ifelse(rx %in% c("black", "b", "bl", "aa", "african american", "afr. amer.", "a.a."), "Black",
                      ifelse(rx %in% c("asian"), "Asian", NA_character_)))
   rx
-}
+})
 # test_case <- c(NA, "BL", "black", "White", "AA", "Asian")
 # handle_race(test_case)
 # 
 
-handle_weight_units <- function(weight_units){
+handle_weight_units <- Vectorize(
+  function(weight_units){
   wu = ifelse(tolower(weight_units) %in% c("lbs","pounds", "lb"), "lbs",
               ifelse(tolower(weight_units) %in% c("kg", "kilograms", "kilog", "KG"), "kg", NA_character_))
   wu
-}
+})
 # test_case <- c("POUNDS", "lbs", "kilog", "kg")
 # handle_weight_units(test_case)
