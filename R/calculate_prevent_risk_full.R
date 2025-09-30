bf47d67ca11cce18749c48e46f3ebd813926e0b8
JMLuther <james.luther@vanderbilt.edu>
2025-09-30 15:45:22 -0500

    update PREVENT risk variable limits

--- R/calculate_prevent_risk_full.R
+++ R/calculate_prevent_risk_full.R
@@ -8,6 +8,14 @@
 #' provided in \href{https://pubmed.ncbi.nlm.nih.gov/37947085/}{Khan et al.,
 #' Circulation. 2024}. This model incorporates urine albumin, A1C and socioeconomic risk (by Zip code) if available. SDI lookup by zipcode is slightly off in SDI estimate- enter the SDI decile (1-10) for most accurate results.
 #'
+#' #' Allowable range for Risk variables are:
+#' *  Age 30-79
+#' *  Total Cholesterol 130-320 mg/dL
+#' *  HDL Cholesterol 20-100 mg/dL
+#' *  SBP 90-200 mmHg
+#' *  BMI 18.5 - 40 mg/m^2
+#' *  eGFR 15-150 mL/min/1.73m^2
+#' *  Urine albumin/Creatinine 0.1-2500 mg/g Creatinine
 #'
 #'@param risk Desired Risk Calculation ("cvd", "ascvd", "hf", "cad", "stroke")
 #'@param gender Gender, (Female/Male)
@@ -109,124 +117,219 @@
 #'                                       UACR, HbA1c, SDI,
 #'                                       using_antihypertensive_medication = using_antihypertensive_medication,
 #'                                       diabetes=diabetes)
-#'    ) 
+#'    )
 #' df_risk
 
-calculate_prevent_risk_full <- function(risk, gender, 
-                                   age, Tc, HDL, SBP, eGFR, 
-                                   BMI=NA, UACR=NA, HbA1c=NA, 
-                                   SDI=NA, zipcode=NA, year=2019,
-                                   current_smoker=FALSE,
-                                   using_antihypertensive_medication=FALSE,
-                                   using_statin=FALSE,
-                                   diabetes=FALSE,
-                                   chol_units = "mg/dL") {
-  
-  BMI = ifelse(is.na(BMI), 0, BMI) # used only in HF calculations
-  SDI =ifelse(!is.na(SDI), SDI,
-              ifelse(!is.na(zipcode), sdi_decile({{zipcode}}, {{year}}), NA))
-  SDI_missing =  ifelse(is.na(SDI) & is.na(zipcode), TRUE, FALSE)
-  SDI4_6 = SDI>=4 & SDI<7
-  SDI7_10 = SDI>=7
-  UACR_missing = ifelse(is.na(UACR), TRUE, FALSE)
-  HbA1c_missing = ifelse(is.na(HbA1c), TRUE, FALSE)
-  Tc = ifelse(chol_units == "mg/dL", Tc*0.02586, Tc) # convert to mmol/L
-  HDL = ifelse(chol_units == "mg/dL", HDL*0.02586, HDL)  # convert to mmol/L
+calculate_prevent_risk_full <- function(
+       risk,
+       gender,
+       age,
+       Tc,
+       HDL,
+       SBP,
+       eGFR,
+       BMI = NA,
+       UACR = NA,
+       HbA1c = NA,
+       SDI = NA,
+       zipcode = NA,
+       year = 2019,
+       current_smoker = FALSE,
+       using_antihypertensive_medication = FALSE,
+       using_statin = FALSE,
+       diabetes = FALSE,
+       chol_units = "mg/dL"
+) {
+       BMI = ifelse(is.na(BMI), 0, BMI) # used only in HF calculations
+       SDI = ifelse(
+              !is.na(SDI),
+              SDI,
+              ifelse(!is.na(zipcode), sdi_decile({{ zipcode }}, {{ year }}), NA)
+       )
+       SDI_missing = ifelse(is.na(SDI) & is.na(zipcode), TRUE, FALSE)
+       SDI4_6 = SDI >= 4 & SDI < 7
+       SDI7_10 = SDI >= 7
+       UACR_missing = ifelse(is.na(UACR), TRUE, FALSE)
+       HbA1c_missing = ifelse(is.na(HbA1c), TRUE, FALSE)
 
-  # validate risk request
-  risk = tolower(risk)
-  risk_query <- 
-    ifelse(risk%in% c("cvd", "cardiovascular disease", "cardiovascular"), "cvd",
-           ifelse(risk%in% c("ascvd", "atherosclerotic cvd"), "ascvd",
-                  ifelse(risk%in% c("hf", "chf", "heart failure", "ahf"), "hf",
-                         ifelse(risk%in% c("cad", "coronary artery disease", "mi", "myocardial infarction", "heart attack"), "cad",
-                                ifelse(risk%in% c("stroke", "cva"), "stroke", NA)))))
-  # validate gender
-  gender_query= tolower(handle_sex(gender))
+       # Replace values out of range with NA_real_:
+       age = ifelse(age < 30, NA_real_, ifelse(age > 79, NA_real_, age))
+       Tc = ifelse(Tc < 130, NA_real_, ifelse(Tc > 320, NA_real_, Tc))
+       HDL = ifelse(HDL < 20, NA_real_, ifelse(HDL > 100, NA_real_, HDL))
+       SBP = ifelse(SBP < 90, NA_real_, ifelse(SBP > 200, NA_real_, SBP))
+       eGFR = ifelse(eGFR < 15, NA_real_, ifelse(eGFR > 150, NA_real_, eGFR))
+       BMI = ifelse(BMI < 18.5, NA_real_, ifelse(BMI > 40, NA_real_, BMI))
+       UACR = ifelse(UACR < 0.1, NA_real_, ifelse(UACR > 25000, NA_real_, UACR))
+       HbA1c = ifelse(
+              HbA1c < 3.0,
+              NA_real_,
+              ifelse(HbA1c > 15, NA_real_, HbA1c)
+       )
 
-  # 10 YEAR BASE MODEL  
-  cf10 <- cfs_full10yr[cfs_full10yr$risk=={{risk_query}} & cfs_full10yr$gender=={{gender_query}}, ]
-  
-  # construct the model
-  log_odds10 = 
-    cf10$constant + 
-    cf10$age*(age-55) /10 + # AGE 
-    cf10$nonHDL*(Tc-HDL-3.5) +  # NON-HDL CHOL
-    cf10$HDL*(HDL-1.3) /0.3 +
-    cf10$sbp_under110*(min(SBP, 110)-110) /20 +
-    cf10$sbp_over110*(max(SBP, 110)-130) /20 + 
-    cf10$diabetes*(diabetes) + 
-    cf10$current_smoker*(current_smoker) + 
-    # BMI used in HF calc only
-    cf10$bmi_under30*(min(BMI, 30)-25) / 5 + 
-    cf10$bmi_over30*(max(BMI, 30)-30) / 5 + 
-    cf10$eGFR_under60*(min(eGFR, 60)-60) / -15 + 
-    cf10$eGFR_over60*(max(eGFR, 60)-90) / -15 + 
-    cf10$using_antihypertensive_medication*(using_antihypertensive_medication)+
-    cf10$using_statin*(using_statin)+
-    cf10$sbp_treated_over110*(using_antihypertensive_medication)*(max(SBP, 110)-130) /20 + 
-    cf10$treated_nonHDL*(using_statin)*(Tc-HDL-3.5) +
-    cf10$age_nonHDL_int*(age-55) /10*(Tc-HDL-3.5) + 
-    cf10$age_HDL_int*(age-55) /10*(HDL-1.3) /0.3 +
-    cf10$age_sbp_int*(age-55) /10*(max(SBP, 110)-130)/20 +
-    cf10$age_diabetes_int*(age-55) /10*(diabetes) +
-    cf10$age_smoking_int*(age-55) /10*(current_smoker) +
-    cf10$age_bmi_int*(age-55)/10*(max(BMI, 30)-30)/5 +
-    cf10$age_egfr_int*(age-55) /10*(min(eGFR, 60)-60) / -15 +
-    ifelse(SDI_missing, cf10$sdi_missing, 
-           ifelse(SDI4_6, cf10$sdi_4_6,
-                  ifelse(SDI7_10, cf10$sdi__10, 0))) +
-    ifelse(UACR_missing, cf10$acr_missing, cf10$ln_acr*log(UACR)) +
-    ifelse(HbA1c_missing, cf10$a1c_missing, 
-           ifelse(diabetes, cf10$a1c_dm*(HbA1c-5.3),
-                  ifelse(!diabetes, cf10$a1c_nondm*(HbA1c-5.3)))) 
+       Tc = ifelse(chol_units == "mg/dL", Tc * 0.02586, Tc) # convert to mmol/L
+       HDL = ifelse(chol_units == "mg/dL", HDL * 0.02586, HDL) # convert to mmol/L
 
-  # # 30 YEAR BASE MODEL  
-  cf30 <- cfs_full30yr[cfs_full30yr$risk=={{risk_query}} & cfs_full30yr$gender=={{gender_query}}, ]
+       # validate risk request
+       risk = tolower(risk)
+       risk_query <-
+              ifelse(
+                     risk %in%
+                            c(
+                                   "cvd",
+                                   "cardiovascular disease",
+                                   "cardiovascular"
+                            ),
+                     "cvd",
+                     ifelse(
+                            risk %in% c("ascvd", "atherosclerotic cvd"),
+                            "ascvd",
+                            ifelse(
+                                   risk %in%
+                                          c(
+                                                 "hf",
+                                                 "chf",
+                                                 "heart failure",
+                                                 "ahf"
+                                          ),
+                                   "hf",
+                                   ifelse(
+                                          risk %in%
+                                                 c(
+                                                        "cad",
+                                                        "coronary artery disease",
+                                                        "mi",
+                                                        "myocardial infarction",
+                                                        "heart attack"
+                                                 ),
+                                          "cad",
+                                          ifelse(
+                                                 risk %in% c("stroke", "cva"),
+                                                 "stroke",
+                                                 NA
+                                          )
+                                   )
+                            )
+                     )
+              )
+       # validate gender
+       gender_query = tolower(handle_sex(gender))
 
-  # construct the model
-  log_odds30 = 
-    cf30$constant + 
-    cf30$age*(age-55) /10 + # AGE 
-    cf30$age_sq*((age-55) /10)^2 + # AGE 
-    cf30$nonHDL*(Tc-HDL-3.5) +  # NON-HDL CHOL
-    cf30$HDL*(HDL-1.3) /0.3 +
-    cf30$sbp_under110*(min(SBP, 110)-110) /20 +
-    cf30$sbp_over110*(max(SBP, 110)-130) /20 + 
-    cf30$diabetes*(diabetes) + 
-    cf30$current_smoker*(current_smoker) + 
-    # BMI used in HF calc only
-    cf30$bmi_under30*(min(BMI, 30)-25) / 5 + 
-    cf30$bmi_over30*(max(BMI, 30)-30) / 5 + 
-    cf30$eGFR_under60*(min(eGFR, 60)-60) / -15 + 
-    cf30$eGFR_over60*(max(eGFR, 60)-90) / -15 + 
-    cf30$using_antihypertensive_medication*(using_antihypertensive_medication)+
-    cf30$using_statin*(using_statin)+
-    cf30$sbp_treated_over110*(using_antihypertensive_medication)*(max(SBP, 110)-130) /20 + 
-    cf30$treated_nonHDL*(using_statin)*(Tc-HDL-3.5) +
-    cf30$age_nonHDL_int*(age-55) /10*(Tc-HDL-3.5) + 
-    cf30$age_HDL_int*(age-55) /10*(HDL-1.3) /0.3 +
-    cf30$age_sbp_int*(age-55) /10*(max(SBP, 110)-130)/20 +
-    cf30$age_diabetes_int*(age-55) /10*(diabetes) +
-    cf30$age_smoking_int*(age-55) /10*(current_smoker) +
-    cf30$age_bmi_int*(age-55)/10*(max(BMI, 30)-30)/5 +
-    cf30$age_egfr_int*(age-55) /10*(min(eGFR, 60)-60) / -15 +
-    ifelse(SDI_missing, cf30$sdi_missing, 
-           ifelse(SDI4_6, cf30$sdi_4_6,
-                  ifelse(SDI7_10, cf30$sdi__10, 0))) +
-    ifelse(UACR_missing, cf30$acr_missing, cf30$ln_acr*log(UACR)) +
-    ifelse(HbA1c_missing, cf30$a1c_missing, 
-           ifelse(diabetes, cf30$a1c_dm*(HbA1c-5.3),
-                  ifelse(!diabetes, cf30$a1c_nondm*(HbA1c-5.3)))) 
-  
-  Risk10 = exp(log_odds10) / (1 + exp(log_odds10))
-  Risk30 = exp(log_odds30) / (1 + exp(log_odds30))
-  res = data.frame(Risk10*100, 
-                   Risk30*100)
-  names(res) <- paste0(risk, c("_10yr", "_30yr"))
-  return(res)
-  
+       # 10 YEAR BASE MODEL
+       cf10 <- cfs_full10yr[
+              cfs_full10yr$risk == {{ risk_query }} &
+                     cfs_full10yr$gender == {{ gender_query }},
+       ]
+
+       # construct the model
+       log_odds10 =
+              cf10$constant +
+              cf10$age * (age - 55) / 10 + # AGE
+              cf10$nonHDL * (Tc - HDL - 3.5) + # NON-HDL CHOL
+              cf10$HDL * (HDL - 1.3) / 0.3 +
+              cf10$sbp_under110 * (min(SBP, 110) - 110) / 20 +
+              cf10$sbp_over110 * (max(SBP, 110) - 130) / 20 +
+              cf10$diabetes * (diabetes) +
+              cf10$current_smoker * (current_smoker) +
+              # BMI used in HF calc only
+              cf10$bmi_under30 * (min(BMI, 30) - 25) / 5 +
+              cf10$bmi_over30 * (max(BMI, 30) - 30) / 5 +
+              cf10$eGFR_under60 * (min(eGFR, 60) - 60) / -15 +
+              cf10$eGFR_over60 * (max(eGFR, 60) - 90) / -15 +
+              cf10$using_antihypertensive_medication *
+                     (using_antihypertensive_medication) +
+              cf10$using_statin * (using_statin) +
+              cf10$sbp_treated_over110 *
+                     (using_antihypertensive_medication) *
+                     (max(SBP, 110) - 130) /
+                     20 +
+              cf10$treated_nonHDL * (using_statin) * (Tc - HDL - 3.5) +
+              cf10$age_nonHDL_int * (age - 55) / 10 * (Tc - HDL - 3.5) +
+              cf10$age_HDL_int * (age - 55) / 10 * (HDL - 1.3) / 0.3 +
+              cf10$age_sbp_int * (age - 55) / 10 * (max(SBP, 110) - 130) / 20 +
+              cf10$age_diabetes_int * (age - 55) / 10 * (diabetes) +
+              cf10$age_smoking_int * (age - 55) / 10 * (current_smoker) +
+              cf10$age_bmi_int * (age - 55) / 10 * (max(BMI, 30) - 30) / 5 +
+              cf10$age_egfr_int * (age - 55) / 10 * (min(eGFR, 60) - 60) / -15 +
+              ifelse(
+                     SDI_missing,
+                     cf10$sdi_missing,
+                     ifelse(
+                            SDI4_6,
+                            cf10$sdi_4_6,
+                            ifelse(SDI7_10, cf10$sdi__10, 0)
+                     )
+              ) +
+              ifelse(UACR_missing, cf10$acr_missing, cf10$ln_acr * log(UACR)) +
+              ifelse(
+                     HbA1c_missing,
+                     cf10$a1c_missing,
+                     ifelse(
+                            diabetes,
+                            cf10$a1c_dm * (HbA1c - 5.3),
+                            ifelse(!diabetes, cf10$a1c_nondm * (HbA1c - 5.3))
+                     )
+              )
+
+       # # 30 YEAR BASE MODEL
+       cf30 <- cfs_full30yr[
+              cfs_full30yr$risk == {{ risk_query }} &
+                     cfs_full30yr$gender == {{ gender_query }},
+       ]
+
+       # construct the model
+       log_odds30 =
+              cf30$constant +
+              cf30$age * (age - 55) / 10 + # AGE
+              cf30$age_sq * ((age - 55) / 10)^2 + # AGE
+              cf30$nonHDL * (Tc - HDL - 3.5) + # NON-HDL CHOL
+              cf30$HDL * (HDL - 1.3) / 0.3 +
+              cf30$sbp_under110 * (min(SBP, 110) - 110) / 20 +
+              cf30$sbp_over110 * (max(SBP, 110) - 130) / 20 +
+              cf30$diabetes * (diabetes) +
+              cf30$current_smoker * (current_smoker) +
+              # BMI used in HF calc only
+              cf30$bmi_under30 * (min(BMI, 30) - 25) / 5 +
+              cf30$bmi_over30 * (max(BMI, 30) - 30) / 5 +
+              cf30$eGFR_under60 * (min(eGFR, 60) - 60) / -15 +
+              cf30$eGFR_over60 * (max(eGFR, 60) - 90) / -15 +
+              cf30$using_antihypertensive_medication *
+                     (using_antihypertensive_medication) +
+              cf30$using_statin * (using_statin) +
+              cf30$sbp_treated_over110 *
+                     (using_antihypertensive_medication) *
+                     (max(SBP, 110) - 130) /
+                     20 +
+              cf30$treated_nonHDL * (using_statin) * (Tc - HDL - 3.5) +
+              cf30$age_nonHDL_int * (age - 55) / 10 * (Tc - HDL - 3.5) +
+              cf30$age_HDL_int * (age - 55) / 10 * (HDL - 1.3) / 0.3 +
+              cf30$age_sbp_int * (age - 55) / 10 * (max(SBP, 110) - 130) / 20 +
+              cf30$age_diabetes_int * (age - 55) / 10 * (diabetes) +
+              cf30$age_smoking_int * (age - 55) / 10 * (current_smoker) +
+              cf30$age_bmi_int * (age - 55) / 10 * (max(BMI, 30) - 30) / 5 +
+              cf30$age_egfr_int * (age - 55) / 10 * (min(eGFR, 60) - 60) / -15 +
+              ifelse(
+                     SDI_missing,
+                     cf30$sdi_missing,
+                     ifelse(
+                            SDI4_6,
+                            cf30$sdi_4_6,
+                            ifelse(SDI7_10, cf30$sdi__10, 0)
+                     )
+              ) +
+              ifelse(UACR_missing, cf30$acr_missing, cf30$ln_acr * log(UACR)) +
+              ifelse(
+                     HbA1c_missing,
+                     cf30$a1c_missing,
+                     ifelse(
+                            diabetes,
+                            cf30$a1c_dm * (HbA1c - 5.3),
+                            ifelse(!diabetes, cf30$a1c_nondm * (HbA1c - 5.3))
+                     )
+              )
+
+       Risk10 = exp(log_odds10) / (1 + exp(log_odds10))
+       Risk30 = exp(log_odds30) / (1 + exp(log_odds30))
+       res = data.frame(Risk10 * 100, Risk30 * 100)
+       names(res) <- paste0(risk, c("_10yr", "_30yr"))
+       return(res)
 }
-
-
-
--- R/sdi_decile.R
+++ R/sdi_decile.R
@@ -1,14 +1,11 @@
 #' Get Social Deprivation Index (SDI) by Zipcode
 #'
 #' Return the SDI decile calculated by 5-year intervals. SDI decile estimates
-#' are available for years ending in 2012, 2015, 2016, 2017, 2018, 2019.
-#' Quantiles are calculated by the SDI score for each year. Detailed description
-#' and source data from
+#' are based on 2015-2019. Detailed description and source data from
 #' \href{https://www.graham-center.org/maps-data-tools/social-deprivation-index.html}{Robert
 #' Graham Center}.
 #'
 #' @param zipcode Zipcode area
-#' @param year Year (default to most recent = 2019)
 #'
 #' @return Single value representing the SDI Decile (lower is better)
 #' @export
@@ -17,6 +14,6 @@
 #' sdi_decile(37220)
 #' sdi_decile(90210)
 
-sdi_decile <- function(zipcode, year=2019){
-  df_sdi[df_sdi$ZCTA5_FIPS=={{zipcode}} & df_sdi$year=={{year}}, ][["sdi_decile"]]
+sdi_decile <- function(zipcode) {
+  df_sdi[df_sdi$ZCTA5_FIPS == {{ zipcode }}, ][["sdi_decile"]]
 }
