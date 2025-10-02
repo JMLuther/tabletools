f841d511befab941290176d95654cebf96d6fe8a
JMLuther <james.luther@vanderbilt.edu>
2025-10-02 12:45:42 -0500

    fix sdi

--- R/sdi_decile.R
+++ R/sdi_decile.R
@@ -18,8 +18,8 @@
 sdi_decile <- function(zipcode) {
   # df_sdi[df_sdi$ZCTA5_FIPS == {{ zipcode }}, ][["sdi_decile"]]
   ifelse(
-    length(df_sdi[ZCTA5_FIPS == 37011, ][["sdi_decile"]] == 0),
+    length(df_sdi[df_sdi$ZCTA5_FIPS == {{ zipcode }}, ][["sdi_decile"]] == 0),
     NA_integer_,
-    df_sdi[ZCTA5_FIPS == 37011, ][["sdi_decile"]]
+    df_sdi[df_sdi$ZCTA5_FIPS == {{ zipcode }}, ][["sdi_decile"]]
   )
 }
